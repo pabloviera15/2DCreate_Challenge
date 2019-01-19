d52d9af6fc41c2fa3706f1a94452dd1ee1fe9c7d
J97cp <juan97_carlos@hotmail.com>
2019-01-19 15:57:50 -0700

    Mas scripts
    Script del piso que se cae

---
+++ CreateChallenge2D/Assets/Scripts/FallingFloorScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingFloorScript : MonoBehaviour
+{
+    public float timeForFall;
+    bool fall;
+    Rigidbody2D rb;
+    public int fallSpeed;
+
+    private void Start()
+    {
+        fall = false;
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            fall = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (fall)
+        {
+            timeForFall -= Time.deltaTime;
+            if(timeForFall <= 0)
+            {
+                rb.gravityScale = fallSpeed;
+            }
+        }
+    }
+}
