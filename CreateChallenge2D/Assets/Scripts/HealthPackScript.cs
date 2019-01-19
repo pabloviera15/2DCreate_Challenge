39ebf3c04141d295172e2352ebdc778f6c2757ce
J97cp <juan97_carlos@hotmail.com>
2019-01-19 15:36:14 -0700

    HealthPack Script
    Yey

---
+++ CreateChallenge2D/Assets/Scripts/HealthPackScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPackScript : MonoBehaviour
+{
+    public int hpRegen;
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.tag == "Player")
+        {
+            collision.GetComponent<PlayerScript>().TakeDamage(hpRegen * -1);
+            Destroy(gameObject);
+            //Particulas, yey!
+            //Sonidos, yey!
+        }
+        
+    }
+}
